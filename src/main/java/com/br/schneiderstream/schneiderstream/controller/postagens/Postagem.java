b8678128199289970f96d2b2d841438b4977b5f3
victobonetti <victorbonettixd@gmail.com>
2023-08-14 22:29:04 -0300

    Class postagem

--- src/main/java/com/br/schneiderstream/schneiderstream/controller/postagens/Postagem.java
+++ src/main/java/com/br/schneiderstream/schneiderstream/controller/postagens/Postagem.java
@@ -1,5 +1,9 @@
 package com.br.schneiderstream.schneiderstream.controller.postagens;
 
 public class Postagem {
-    
+    private int id;
+    private String titulo;
+    private String userId;
+    private String conteudo;
+    private Long imagem;
 }
