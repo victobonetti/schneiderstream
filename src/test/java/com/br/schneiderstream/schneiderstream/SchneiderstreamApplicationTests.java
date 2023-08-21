06a3e7758d556729392234a6bd2f2b27eb54c120
victobonetti <victorbonettixd@gmail.com>
2023-08-20 22:57:58 -0300

    STARTING TESTS

--- src/test/java/com/br/schneiderstream/schneiderstream/SchneiderstreamApplicationTests.java
+++ src/test/java/com/br/schneiderstream/schneiderstream/SchneiderstreamApplicationTests.java
@@ -1,13 +1,50 @@
 package com.br.schneiderstream.schneiderstream;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SchneiderstreamApplicationTests {
 
+	@Autowired
+	private MockMvc m;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@Test
-	void contextLoads() {
+	void testGetUsers() throws Exception {
+		MvcResult result = m.perform(get("/users"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andReturn();
+
+		String jsonResponse = result.getResponse().getContentAsString();
+		int arraySize = JsonPath.read(jsonResponse, "$.content.length()");
+
+		if (arraySize >= 1) {
+			m.perform(get("/users"))
+					.andExpect(jsonPath("$.content[0].id").isNumber());
+		}
+	};
+
+	@Test
+	void testCreateUser() throws Exception {
+		
 	}
 
 }
