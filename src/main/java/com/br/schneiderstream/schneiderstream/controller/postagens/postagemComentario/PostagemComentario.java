c0185f56339980a3de2c7d70519f26401a580922
victobonetti <victorbonettixd@gmail.com>
2023-08-27 00:16:15 -0300

    PostagemComentario

---
+++ src/main/java/com/br/schneiderstream/schneiderstream/controller/postagens/postagemComentario/PostagemComentario.java
@@ -0,0 +1,14 @@
+package com.br.schneiderstream.schneiderstream.controller.postagens.postagemComentario;
+
+public class PostagemComentario {
+
+    private String id;
+    private String userId;
+    private String postId;
+    private String conteudo;
+
+
+    public PostagemComentario(PostagemComentarioDto comentario){
+
+    }
+}
